fc4c9b149a2ff36bf1c6b5aaac681689e3e0f2fe
Hyun Yi <hy00un__@naver.com>
2023-06-29 23:19:58 +0900

    refactor: type 지정 코드 수정

--- src/main/java/com/water9tree/streetlamp/lamp/dto/request/LampSearchRequest.java
+++ src/main/java/com/water9tree/streetlamp/lamp/dto/request/LampSearchRequest.java
@@ -1,12 +1,12 @@
 package com.water9tree.streetlamp.lamp.dto.request;
 
-import com.water9tree.streetlamp.lamp.entity.Lamp;
+import com.water9tree.streetlamp.lamp.entity.Lamp.Status;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record LampSearchRequest(
     @NotNull(message = "상태를 입력해주세요.")
-    Lamp.Status status) {
+    Status status) {
 
 }
