f9a945b878c4a9749edd02df9da0a1b9383d6472
publdaze <publdaze@naver.com>
2023-07-21 01:35:39 +0900

    feat: swagger로 api 문서화

--- src/lamps/lamps.controller.ts
+++ src/lamps/lamps.controller.ts
@@ -12,16 +12,25 @@ import { LampsService } from './lamps.service';
 import { CreateLampDto } from './dtos/create-lamp.dto';
 import { UpdateLampDto } from './dtos/update-lamp.dto';
 import { ObjectId } from 'mongoose';
+import { ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller('lamps')
 export class LampsController {
   constructor(private readonly lampsService: LampsService) {}
 
+  @ApiQuery({
+    name: 'status',
+    required: false,
+  })
   @Get()
   getLamps(@Query('status') status: 'light' | 'dark' | null) {
     return this.lampsService.getLamps(status);
   }
 
+  @ApiParam({
+    name: 'lampId',
+    type: String,
+  })
   @Get(':lampId')
   getLampById(@Param('lampId') lampId: ObjectId) {
     return this.lampsService.getLampById(lampId);
@@ -32,6 +41,10 @@ export class LampsController {
     return this.lampsService.createLamp(lampData);
   }
 
+  @ApiParam({
+    name: 'lampId',
+    type: String,
+  })
   @Patch(':lampId')
   updateLamp(
     @Param('lampId') lampId: ObjectId,
@@ -40,6 +53,10 @@ export class LampsController {
     return this.lampsService.updateLamp(lampId, updatedLampData);
   }
 
+  @ApiParam({
+    name: 'lampId',
+    type: String,
+  })
   @Delete(':lampId')
   deleteLamp(@Param('lampId') lampId: ObjectId) {
     return this.lampsService.deleteLamp(lampId);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Lamp API')
+    .setDescription('Lamp API 입니다.')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
