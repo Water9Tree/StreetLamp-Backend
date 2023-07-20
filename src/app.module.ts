da96517e43b507929ca7cab029cdb553a9ac0e58
publdaze <publdaze@naver.com>
2023-07-20 22:27:14 +0900

    feat: DB 연결 기초 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LampsModule } from './lamps/lamps.module';
+
 @Module({
-  imports: [LampsModule],
+  imports: [
+    LampsModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
