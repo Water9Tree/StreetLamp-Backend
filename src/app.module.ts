4adb5e9b2f477ed5157d9c0c2647b087b956cb6d
publdaze <publdaze@naver.com>
2023-07-26 22:48:57 +0900

    feat: generate users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LampsModule } from './lamps/lamps.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { LampsModule } from './lamps/lamps.module';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
